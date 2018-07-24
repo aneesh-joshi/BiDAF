3358979aa83f696a73884d411031b5e4b73842ea
Aneesh <aneeshyjoshi@gmail.com>
2018-07-24 17:42:23 +0000

    allow testing

--- my_try.py
+++ my_try.py
@@ -155,6 +155,101 @@ class MyWikiIterable:
                     n_filtered_docs += 1
                     n_relevant_docs = 0
 
+class MyOtherWikiIterable:
+
+    def __init__(self, fpath):
+        # self.type_translator = {'query': 0, 'doc': 1, 'label': 2}
+        # self.iter_type = iter_type
+        with open(fpath, encoding='utf8') as tsv_file:
+            tsv_reader = csv.reader(tsv_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+            self.data_rows = []
+            for row in tsv_reader:
+                self.data_rows.append(row)
+        self.to_print = ""
+
+    def preprocess_sent(self, sent):
+        """Utility function to lower, strip and tokenize each sentence
+
+        Replace this function if you want to handle preprocessing differently"""
+        return re.sub("[^a-zA-Z0-9]", " ", sent.strip().lower()).split()
+
+    def  get_stuff(self):
+        # Defining some consants for .tsv reading
+        QUESTION_ID_INDEX = 0
+        QUESTION_INDEX = 1
+        ANSWER_INDEX = 5
+        ANSWER_ID_INDEX = 4
+        LABEL_INDEX = 6
+
+        document_group = []
+        label_group = []
+
+        n_relevant_docs = 0
+        n_filtered_docs = 0
+
+        query_ids = []
+        query_id_group = []
+        doc_ids = []
+        doc_id_group = []
+        queries = []
+        docs = []
+        labels = []
+
+        for i, line in enumerate(self.data_rows[1:], start=1):
+            if i < len(self.data_rows) - 1:  # check if out of bounds might occur
+                if self.data_rows[i][QUESTION_ID_INDEX] == self.data_rows[i + 1][QUESTION_ID_INDEX]:
+                    document_group.append(self.preprocess_sent(self.data_rows[i][ANSWER_INDEX]))
+                    doc_ids.append(self.data_rows[i][ANSWER_ID_INDEX])
+
+                    label_group.append(int(self.data_rows[i][LABEL_INDEX]))
+
+                    n_relevant_docs += int(self.data_rows[i][LABEL_INDEX])
+                else:
+                    document_group.append(self.preprocess_sent(self.data_rows[i][ANSWER_INDEX]))
+                    doc_ids.append(self.data_rows[i][ANSWER_ID_INDEX])
+
+                    label_group.append(int(self.data_rows[i][LABEL_INDEX]))
+
+
+                    n_relevant_docs += int(self.data_rows[i][LABEL_INDEX])
+
+                    if n_relevant_docs > 0:
+                        docs.append(document_group)
+                        labels.append(label_group)
+                        queries.append(self.preprocess_sent(self.data_rows[i][QUESTION_INDEX]))
+
+                        query_ids.append(self.data_rows[i][QUESTION_ID_INDEX])
+                        doc_id_group.append(doc_ids)
+                    else:
+                        n_filtered_docs += 1
+
+                    n_relevant_docs = 0
+                    document_group = []
+                    label_group = []
+                    doc_ids = []
+            else:
+                # If we are on the last line
+                document_group.append(self.preprocess_sent(self.data_rows[i][ANSWER_INDEX]))
+                label_group.append(int(self.data_rows[i][LABEL_INDEX]))
+
+                doc_ids.append(self.data_rows[i][ANSWER_ID_INDEX])
+
+
+                n_relevant_docs += int(self.data_rows[i][LABEL_INDEX])
+
+                if n_relevant_docs > 0:
+                    docs.append(document_group)
+                    labels.append(label_group)
+                    queries.append(self.preprocess_sent(self.data_rows[i][QUESTION_INDEX]))
+                    doc_id_group.append(doc_ids)
+                    query_ids.append(self.data_rows[i][QUESTION_ID_INDEX])
+                else:
+                    n_filtered_docs += 1
+                    n_relevant_docs = 0
+        return queries, docs, labels, query_ids, doc_id_group
+
+
+
 if __name__ == '__main__':
     q_iterable = MyWikiIterable('query', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-train.tsv'))
     d_iterable = MyWikiIterable('doc', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-train.tsv'))
@@ -186,5 +281,14 @@ if __name__ == '__main__':
 
     kv_model = api.load('glove-wiki-gigaword-50')
     model = DRMM_TKS(q_iterable, d_iterable, l_iterable, kv_model, text_maxlen=40, unk_handle_method='zero', epochs=2)
-    model.predict(q_test_iterable, d_test_iterable, l_test_iterable)
+    #model.predict(q_test_iterable, d_test_iterable, l_test_iterable)
+    #print(model.tiny_predict('Hello there', 'general kenobi'))
+    queries, doc_group, label_group, query_ids, doc_id_group = MyOtherWikiIterable(os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-test.tsv')).get_stuff()
+    with open('jpred', 'w') as f:
+        for q, doc, labels, q_id, d_ids in zip(queries, doc_group, label_group, query_ids, doc_id_group):
+            for d, l, d_id in zip(doc, labels, d_ids):
+                my_score = str(model.tiny_predict(q,d))
+                f.write(q_id + '\t' + 'Q0' + '\t' + str(d_id) + '\t' + '99' + '\t' + my_score + '\t' + 'STANDARD' + '\n')
+    print("Prediction done. Saved as %s" % 'jpred')
+
 

4cb255a211f8d34ea9ebbd1d0f1e702c23922f62
aneesh-joshi <aneeshyjoshi@gmail.com>
2018-07-26 16:53:22 +0530

    refactor

--- my_try.py
+++ my_try.py
@@ -1,86 +1,9 @@
 import gensim.downloader as api
 import re
-from my_model import DRMM_TKS
+from bidaf import BiDAF
 import os
 import csv
 
-def save_qrels(fname):
-    """Saves the WikiQA data `Truth Data`. This remains the same regardless of which model you use.
-    qrels : query relevance
-
-    Format
-    ------
-    <query_id>\t<0>\t<document_id>\t<relevance>
-
-    Note: parameter <0> is ignored by the model
-
-    Example
-    -------
-    Q1  0   D1-0    0
-    Q1  0   D1-1    0
-    Q1  0   D1-2    0
-    Q1  0   D1-3    1
-    Q1  0   D1-4    0
-    Q16 0   D16-0   1
-    Q16 0   D16-1   0
-    Q16 0   D16-2   0
-    Q16 0   D16-3   0
-    Q16 0   D16-4   0
-
-    Parameters
-    ----------
-    fname : str
-        File where the qrels should be saved
-
-    """
-    with open(fname, 'w') as f:
-        for q, doc, labels, q_id, d_ids in zip(queries, doc_group, label_group, query_ids, doc_id_group):
-            for d, l, d_id in zip(doc, labels, d_ids):
-                f.write(q_id + '\t' +  '0' + '\t' +  str(d_id) + '\t' + str(l) + '\n')
-    print("qrels done. Saved as %s" % fname)
-
-def save_model_pred(fname, similarity_fn):
-    """Goes through all the queries and docs, gets their Similarity score as per the `similarity_fn`
-    and saves it in the TREC format
-
-    Format
-    ------
-    <query_id>\t<Q0>\t<document_id>\t<rank>\t<model_score>\t<STANDARD>
-
-    Note: parameters <Q0>, <rank> and <STANDARD> are ignored by the model and can be kept as anything
-    I have chose 99 as the rank. It has no meaning.
-
-    Example
-    -------
-    Q1  Q0  D1-0    99  0.64426434  STANDARD
-    Q1  Q0  D1-1    99  0.26972288  STANDARD
-    Q1  Q0  D1-2    99  0.6259719   STANDARD
-    Q1  Q0  D1-3    99  0.8891963   STANDARD
-    Q1  Q0  D1-4    99  1.7347554   STANDARD
-    Q16 Q0  D16-0   99  1.1078827   STANDARD
-    Q16 Q0  D16-1   99  0.22940424  STANDARD
-    Q16 Q0  D16-2   99  1.7198141   STANDARD
-    Q16 Q0  D16-3   99  1.7576259   STANDARD
-    Q16 Q0  D16-4   99  1.548423    STANDARD
-
-    Parameters
-    ----------
-    fname : str
-        File where the qrels should be saved
-
-    similarity_fn : function
-        Parameters
-            - query : list of str
-            - doc : list of str
-        Returns
-            - similarity_score : float
-    """
-    with open(fname, 'w') as f:
-        for q, doc, labels, q_id, d_ids in zip(queries, doc_group, label_group, query_ids, doc_id_group):
-            for d, l, d_id in zip(doc, labels, d_ids):
-                my_score = str(similarity_fn(q,d))
-                f.write(q_id + '\t' + 'Q0' + '\t' + str(d_id) + '\t' + '99' + '\t' + my_score + '\t' + 'STANDARD' + '\n')
-    print("Prediction done. Saved as %s" % fname)
 
 class MyWikiIterable:
     def __init__(self, iter_type, fpath):
@@ -156,7 +79,7 @@ class MyWikiIterable:
                     n_relevant_docs = 0
 
 class MyOtherWikiIterable:
-
+    """Slight modification over old one for testing"""
     def __init__(self, fpath):
         # self.type_translator = {'query': 0, 'doc': 1, 'label': 2}
         # self.iter_type = iter_type
@@ -251,15 +174,7 @@ class MyOtherWikiIterable:
 
 
 if __name__ == '__main__':
-    q_iterable = MyWikiIterable('query', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-train.tsv'))
-    d_iterable = MyWikiIterable('doc', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-train.tsv'))
-    l_iterable = MyWikiIterable('label', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-train.tsv'))
 
-    q_test_iterable = MyWikiIterable('query', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-test.tsv'))
-    d_test_iterable = MyWikiIterable('doc', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-test.tsv'))
-    l_test_iterable = MyWikiIterable('label', os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-test.tsv'))
-
-    
     q_iterable = MyWikiIterable('query', os.path.join('experimental_data', 'SQUAD-T-QA.tsv'))
     d_iterable = MyWikiIterable('doc', os.path.join('experimental_data', 'SQUAD-T-QA.tsv'))
     l_iterable = MyWikiIterable('label', os.path.join('experimental_data', 'SQUAD-T-QA.tsv'))    
@@ -270,25 +185,23 @@ if __name__ == '__main__':
         doc_lens.append(len(doc))
         for d in doc:
             d_lens.append(len(d))
+
+    print('max query, max number of docs per query and max number of docs')
     print(max(q_lens), max(doc_lens), max(d_lens))
-
-
     import numpy as np
-
     q_lens = np.array(q_lens)
     d_lens = np.array(d_lens)
     doc_lens = np.array(doc_lens)
-
+    print('Average query, average number of docs per query and average number of docs')
     print(np.mean(q_lens), np.mean(doc_lens), np.mean(d_lens))
-
-
-    # exit()
 
     kv_model = api.load('glove-wiki-gigaword-50')
     model = DRMM_TKS(q_iterable, d_iterable, l_iterable, kv_model, text_maxlen=51, unk_handle_method='zero', epochs=3, batch_size=20)
-    #model.predict(q_test_iterable, d_test_iterable, l_test_iterable)
+
+    # Example of how prediction works
     print('Hello there result: ', model.tiny_predict('Hello there', 'general kenobi'))
     print('Hello there batch: ', model.batch_tiny_predict('Hello there', ['gengeral kenowbi', 'i am groot', 'I dont wear boot']))
+
     queries, doc_group, label_group, query_ids, doc_id_group = MyOtherWikiIterable(os.path.join('experimental_data', 'WikiQACorpus', 'WikiQA-test.tsv')).get_stuff()
     i=0
 
---
+++ readme.md
@@ -0,0 +1,24 @@
+# BiDirectional Attention Flow for QA Transfer
+
+This repo contains code which attempts to reproduce the [QA-Transfer Paper](http://aclweb.org/anthology/P17-2081)
+
+The BiDAF model has been largely adapted from (this implementation)[https://github.com/allenai/deep_qa/blob/master/deep_qa/models/reading_comprehension/bidirectional_attention.py] by AllenAI.
+
+The model pre-trains on SQUAD-T dataset. Then it is evaluated on the WikiQA test set.
+
+The SQUAD-T data set has been extracted from the SQUAD dataset and is presented in this repo.
+
+## To run my experiment
+$ python my_try.py
+
+The above script will run the bidaf model on SQUAD-T and evaluate it with WikiQA test set.
+
+
+## Points of interest
+
+- The model is defined in the function _get_keras_model in bidaf.py
+- The training is done from the function train in bidaf.py
+
+
+## Disclaimer
+Code has been reused from old DRMM TKS code. So docstrings are faulty. The old code is only responsible for word indexing, etc.
